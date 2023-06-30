31ebf16bbe81e2d4b9f63c167917ec576e692ee3
Mellomanprost <anoppa@mail.ru>
2023-06-30 13:14:16 +0300

    Added business models

---
+++ SomeNewsBlog.BLL/Models/RoleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeNewsBlog.BLL.Models
+{
+    public class RoleModel
+    {
+        public string? Description { get; set; }
+
+        public RoleModel(RoleModel role)
+        {
+            this.Description = role.Description;
+        }
+    }
+}
