143c3f4f4a6531b565a194e94c475b6cc5d6618a
Mellomanprost <anoppa@mail.ru>
2023-07-24 21:11:06 +0300

    try to add exception handler

--- SNBProject/Controllers/HomeController.cs
+++ SNBProject/Controllers/HomeController.cs
@@ -2,12 +2,14 @@
 using System.Diagnostics;
 using SNBProject.ViewModels;
 using SNB.BLL.Services.IServices;
+using NLog;
 
 namespace SNBProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IHomeService _homeService;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         public HomeController(IHomeService homeService)
         {
@@ -27,9 +29,63 @@ namespace SNBProject.Controllers
         }
 
         [Route("Home/Error")]
-        public IActionResult Error()
+        public IActionResult Error(int? statusCode = null)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode.HasValue)
+            {
+                if (statusCode == 400 || statusCode == 404)
+                {
+                    var viewName = statusCode.ToString();
+                    Logger.Error($"Произошла ошибка - {statusCode}\n{viewName}");
+                    return View(viewName);
+                }
+                else 
+                    if (statusCode == 403)
+                        return View("403");
+
+                    return View("400");
+            }
+            return View("400");
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //generate error 400
+        public IActionResult Error400()
+        {
+            try
+            {
+                throw new HttpRequestException("400");
+            }
+            catch
+            {
+                return View("400");
+            }
+        }
+
+        //generate error 403
+        public IActionResult Error403()
+        {
+            try
+            {
+                throw new HttpRequestException("403");
+            }
+            catch
+            {
+                return View("403");
+            }
+        }
+
+        //generate error 404
+        public IActionResult Error404()
+        {
+            try
+            {
+                throw new HttpRequestException("404");
+            }
+            catch
+            {
+                return View("404");
+            }
         }
     }
 }
--- SNBProject/Program.cs
+++ SNBProject/Program.cs
@@ -56,7 +56,7 @@ namespace SNBProject
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -68,6 +68,7 @@ namespace SNBProject
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
 
             app.MapControllerRoute(
                 name: "default",
